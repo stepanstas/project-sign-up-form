3a70ac6cddccfd64cde569b5bf7e551ee684eaa0
Stepan Stastny <stepan86@yahoo.com>
2023-03-14 12:48:41 -0400

    Refactor code

--- script.js
+++ script.js
@@ -13,14 +13,16 @@ const isValidPassword = (password) => /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(
 
 const clearError = (input, index) => {
     errorMessages[index].textContent = "";
-    input.style.border = "";
+    input.style.borderBlock = "";
+    input.style.borderInline = ""
     input.style.backgroundColor = "";
 }
 
 firstName.addEventListener("input", () => {
     if (!isValidName(firstName.value.trim())) {
       errorMessages[0].textContent = "Please enter a valid first name";
-      firstName.style.border = "3px solid #ff0000";
+      firstName.style.borderBlock = "3px solid #ff0000";
+      firstName.style.borderInline = "3px solid #ff0000";
       firstName.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
     } else {
       clearError(firstName, 0);
@@ -30,7 +32,8 @@ firstName.addEventListener("input", () => {
 lastName.addEventListener("input", () => {
     if (!isValidName(lastName.value.trim())) {
       errorMessages[1].textContent = "Please enter a valid last name";
-      lastName.style.border = "3px solid #ff0000";
+      lastName.style.borderBlock = "3px solid #ff0000";
+      lastName.style.borderInline = "3px solid #ff0000";
       lastName.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
     } else {
       clearError(lastName, 1);
@@ -40,7 +43,8 @@ lastName.addEventListener("input", () => {
 email.addEventListener("input", () => {
     if (!isValidEmail(email.value.trim())) {
       errorMessages[2].textContent = "Please enter a valid email address";
-      email.style.border = "3px solid #ff0000";
+      email.style.borderBlock = "3px solid #ff0000";
+      email.style.borderInline = "3px solid #ff0000";
       email.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
     } else {
       clearError(email, 2);
@@ -50,7 +54,8 @@ email.addEventListener("input", () => {
 phone.addEventListener("input", () => {
     if (!isValidPhone(phone.value.trim())) {
       errorMessages[3].textContent = "Please enter a valid format XXX-XXX-XXXX";
-      phone.style.border = "3px solid #ff0000";
+      phone.style.borderBlock = "3px solid #ff0000";
+      phone.style.borderInline = "3px solid #ff0000";
       phone.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
     } else {
       clearError(phone, 3);
@@ -60,7 +65,8 @@ phone.addEventListener("input", () => {
 password.addEventListener("input", () => {
     if (!isValidPassword(password.value.trim())) {
       errorMessages[4].textContent = "Password must have at least 8 characters, one uppercase letter, one lowercase letter, and one number or special character)";
-      password.style.border = "3px solid #ff0000";
+      password.style.borderBlock = "3px solid #ff0000";
+      password.style.borderInline = "3px solid #ff0000";
       password.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
     } else {
       clearError(password, 4);
@@ -73,57 +79,67 @@ form.addEventListener("submit", (event) => {
     let isValid = true;
 
     if(firstName.value.trim() === "" || firstName.value.trim() === null) {
-        firstName.style.border = "3px solid #ff0000";
+        firstName.style.borderBlock = "3px solid #ff0000";
+        firstName.style.borderInline = "3px solid #ff0000";
         firstName.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
         errorMessages[0].textContent = "Please enter a first name";
         isValid = false;
     } else {
         errorMessages[0].textContent = "";
-        firstName.style.border = "";
+        firstName.style.borderBlock = "";
+        firstName.style.borderInline = "";
         firstName.style.backgroundColor = "";
     }
 
     if(lastName.value.trim() === "" || lastName.value.trim() === null) {
-        lastName.style.border = "3px solid #ff0000";
+        lastName.style.borderBlock = "3px solid #ff0000";
+        lastName.style.borderInline = "3px solid #ff0000";
         lastName.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
         errorMessages[1].textContent = "Please enter a last name";
         isValid = false;
     } else {
         errorMessages[1].textContent = "";
-        lastName.style.border = "";
+        lastName.style.borderBlock = "";
+        lastName.style.borderInline = "";
         lastName.style.backgroundColor = "";
     }
 
     if(!isValidEmail(email.value.trim())) {
-        email.style.border = "3px solid #ff0000";
+        email.style.borderBlock = "3px solid #ff0000";
+        email.style.borderInline = "3px solid #ff0000";
         email.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
         errorMessages[2].textContent = "Please enter a valid email address";
         isValid = false;
     } else {
         errorMessages[2].textContent = "";
-        email.style.border = "";
+        email.style.borderBlock = "";
+        email.style.borderInline = "";
         email.style.backgroundColor = "";
     }
     
     if(!isValidPhone(phone.value.trim())) {
-        phone.style.border = "3px solid #ff0000";
+        phone.style.borderBlock = "3px solid #ff0000";
+        phone.style.borderInline = "3px solid #ff0000";
         phone.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
         errorMessages[3].textContent = "Please enter a valid format XXX-XXX-XXXX";
         isValid = false;
     } else {
         errorMessages[3].textContent = "";
-        phone.style.border = "";
+        phone.style.borderBlock = "";
+        phone.style.borderInline = "";
         phone.style.backgroundColor = "";
     }
     
     if(!isValidPassword(password.value.trim())) {
-        password.style.border = "3px solid #ff0000";
+        password.style.borderBlock = "3px solid #ff0000";
+        password.style.borderInline = "3px solid #ff0000";
         password.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
         errorMessages[4].textContent = "Password must have at least 8 characters, one uppercase letter, one lowercase letter, and one number or special character)";
         isValid = false;
     } else {
         errorMessages[4].textContent = "";
-        password.style.border = "";
+        password.style.borderBlock = "";
+        password.style.borderInline = "";
         password.style.backgroundColor = "";
 
     }
